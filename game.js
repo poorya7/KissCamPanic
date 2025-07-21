84cf83c4dc379b9c5c84192a538830bd32237bd2
poorya piroozan <hellhammervs@gmail.com>
2025-07-20 20:29:16 -0400

    added briefcase throw

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-let player, hr, cursors, kissCam, crowdGroup, projectiles, facingRight = true;
+let player, hr, cursors, kissCam, crowdGroup, projectiles, facingRight = true, shootToggle = false;
 
 function preload() {
   this.load.image("ceo1", "sprites/ceo1.png");
@@ -29,11 +29,12 @@ function preload() {
   this.load.image("shirt", "sprites/crowd/shirt.png");
   this.load.image("pants", "sprites/crowd/pants.png");
   this.load.image("credit_card", "sprites/cc.png");
+  this.load.image("briefcase", "sprites/case.png");
 }
 
 function create() {
   cursors = this.input.keyboard.createCursorKeys();
-  this.input.keyboard.on("keydown-SPACE", shootCreditCard, this);
+  this.input.keyboard.on("keydown-SPACE", shootProjectile, this);
 
   player = this.physics.add.sprite(100, 100, "ceo1").setScale(0.07);
   player.body.setCollideWorldBounds(true);
@@ -94,34 +95,64 @@ function randomColor() {
   );
 }
 
-function shootCreditCard() {
-  const offsetX = facingRight ? 10 : -10;
-  const card = projectiles.create(player.x + offsetX, player.y, "credit_card").setScale(0.02);
+function shootProjectile() {
+  let type = shootToggle ? "credit_card" : "briefcase";
+  shootToggle = !shootToggle;
 
-  card.startY = player.y;
+  let spawnX, spawnY;
+  let originY;
 
-  const speed = 400;
-  const angleDeg = facingRight ? -50 : 230;
-  const angleRad = Phaser.Math.DegToRad(angleDeg);
+  if (type === "credit_card") {
+    const offsetX = facingRight ? 10 : -10;
+    spawnX = player.x + offsetX;
+    spawnY = player.y;
+    originY = player.y;
+  } else {
+    const offsetX = facingRight ? 8 : -8;
+    spawnX = hr.x + offsetX;
+    spawnY = hr.y;
+    originY = hr.y;
+  }
 
-  const velX = Math.cos(angleRad) * speed;
-  const velY = Math.sin(angleRad) * speed;
+  const proj = projectiles.create(spawnX, spawnY, type).setScale(type === "credit_card" ? 0.015 : 0.06);
+  proj.startY = originY;
+  proj.throwerY = originY;
+  proj.type = type;
 
-  card.body.setVelocityX(velX);
-  card.body.setVelocityY(velY);
-  card.body.setAllowGravity(true);
-  card.body.setGravityY(1300);
+  if (type === "credit_card") {
+    const speed = 400;
+    const angleDeg = facingRight ? -50 : 230;
+    const angleRad = Phaser.Math.DegToRad(angleDeg);
+    const velX = Math.cos(angleRad) * speed;
+    const velY = Math.sin(angleRad) * speed;
 
-  card.body.onWorldBounds = true;
-  card.body.world.on("worldbounds", body => {
-    if (body.gameObject === card) {
-      card.destroy();
+    proj.body.setVelocityX(velX);
+    proj.body.setVelocityY(velY);
+    proj.body.setAllowGravity(true);
+    proj.body.setGravityY(1300);
+  } else if (type === "briefcase") {
+    const speed = 400;
+    const angleDeg = facingRight ? -20 : 200;
+    const angleRad = Phaser.Math.DegToRad(angleDeg);
+    const velX = Math.cos(angleRad) * speed;
+    const velY = Math.sin(angleRad) * speed;
+
+    proj.body.setVelocityX(velX);
+    proj.body.setVelocityY(velY);
+    proj.body.setAllowGravity(true);
+    proj.body.setGravityY(1300);
+  }
+
+  proj.body.onWorldBounds = true;
+  proj.body.world.on("worldbounds", body => {
+    if (body.gameObject === proj) {
+      proj.destroy();
     }
   });
 }
 
-function projectileHitsCrowd(card, crowd) {
-  card.destroy();
+function projectileHitsCrowd(proj, crowd) {
+  proj.destroy();
   if (crowd.visuals) crowd.visuals.destroy();
   crowd.destroy();
 }
@@ -166,7 +197,7 @@ function update() {
   hr.x = player.x - 10;
   hr.y = player.y + 10;
 
-  const chaseSpeed = 80;
+  const chaseSpeed = 20;
   kissCam.body.setVelocity(
     kissCam.x < player.x ? chaseSpeed : kissCam.x > player.x ? -chaseSpeed : 0,
     kissCam.y < player.y ? chaseSpeed : kissCam.y > player.y ? -chaseSpeed : 0
@@ -179,17 +210,16 @@ function update() {
     }
   });
 
-  projectiles.getChildren().forEach(card => {
-    // Spin around Z axis
-    card.rotation += 0.3;
-
-    // Fake 3D flip using scaleX oscillation
-    const flip = Math.abs(Math.sin(this.time.now * 0.02));
-    card.setScale(0.02 * flip, 0.02);
-
-    // Auto-destroy when falling below startY
-    if (card.y > card.startY) {
-      card.destroy();
+  projectiles.getChildren().forEach(proj => {
+    if (proj.type === "credit_card") {
+      proj.rotation += 0.3;
+      const flip = Math.abs(Math.sin(this.time.now * 0.02));
+      proj.setScale(0.015 * flip, 0.015);
+      if (proj.y > proj.startY) proj.destroy();
+    } else if (proj.type === "briefcase") {
+      if (proj.y >= proj.throwerY + 5) {
+        proj.destroy();
+      }
     }
   });
 }
